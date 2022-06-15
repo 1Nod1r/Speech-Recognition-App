177525ee689a1f76dd3d179ee798dba7cce81514
1Nod1r <kh.nodirbeek@gmail.com>
2022-06-15 16:44:10 +0500

    Done

--- Speech Recognition App/ViewController.swift
+++ Speech Recognition App/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import InstantSearchVoiceOverlay
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, VoiceOverlayDelegate {
 
+    @IBOutlet var mybutton: UIButton!
+    
+    let voiceOverlay = VoiceOverlayController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mybutton.backgroundColor = .systemRed
+        mybutton.tintColor = .white
     }
+    
+    @IBAction func didTapButton(){
+        voiceOverlay.delegate = self
+        voiceOverlay.settings.autoStart = false
+        voiceOverlay.settings.autoStop = true
+        voiceOverlay.settings.autoStopTimeout = 5
+        voiceOverlay.start(on: self) { text, final, _ in
+            if final {
+                
+            }
+            else {
+                
+            }
+        } errorHandler: { error in
+            
+        }
 
+    }
+    func recording(text: String?, final: Bool?, error: Error?) {
+        
+    }
 
 }
 
